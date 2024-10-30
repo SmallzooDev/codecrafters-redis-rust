1cfd7cd3484122665e9b1a3b474a3010454d426f
smallzooDev <smallzooDev@gmail.com>
2024-10-30 23:27:05 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,44 @@
-use std::io::{Read, Write};
-use std::net::TcpListener;
-use std::str::from_utf8;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::task;
 
-fn main() {
-    println!("Logs from your program will appear here!");
-
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let mut buffer = [0; 512];
-
-                loop {
-                    buffer.fill(0);
-                    match stream.read(&mut buffer) {
-                        Ok(0) => break,
-                        Ok(_) => {
-                            let message = from_utf8(&buffer).unwrap_or("");
-
-                            if message.starts_with("*1\r\n$4\r\nPING\r\n") {
-                                stream.write_all(b"+PONG\r\n").unwrap();
-                            }
-                        },
-                        Err(_) => break,
-                    }
-                }
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                task::spawn(async move {
+                    handle_client(stream).await;
+                });
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error accepting connection : {}", e);
             }
+        }
+    }
+}
+
+async fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    loop {
+        buffer.fill(0);
+        match stream.read(&mut buffer).await {
+            Ok(0) => break,
+            Ok(n) => {
+                let message = match std::str::from_utf8(&buffer[..n]) {
+                   Ok(msg) => msg,
+                    Err(_) => {
+                        println!("Failed to parse message");
+                        continue;
+                    }
+                };
+
+                if message.starts_with("*1\r\n$4\r\nPING\r\n") {
+                    stream.write_all(b"+PONG\r\n").await.unwrap();
+                }
+            },
+            Err(_) => break,
         }
     }
 }
