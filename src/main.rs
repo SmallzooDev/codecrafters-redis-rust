1b0176ba71a5ac8d2229b05f7968cc7bf25f3d34
smallzooDev <smallzooDev@gmail.com>
2024-10-30 20:16:30 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-#![allow(unused_imports)]
-
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::str::from_utf8;
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -12,7 +11,22 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream.write_all(b"+PONG\r\n").unwrap();
+                let mut buffer = [0; 512];
+
+                loop {
+                    buffer.fill(0);
+                    match stream.read(&mut buffer) {
+                        Ok(0) => break,
+                        Ok(_) => {
+                            let message = from_utf8(&buffer).unwrap_or("");
+
+                            if message.starts_with("*1\r\n$4\r\nPING\r\n") {
+                                stream.write_all(b"+PONG\r\n").unwrap();
+                            }
+                        },
+                        Err(_) => break,
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
