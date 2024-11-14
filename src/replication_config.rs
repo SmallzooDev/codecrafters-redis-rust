c0ad5fb073d020158c85c4d99cdf6feb8835b197
smallzooDev <smallzooDev@gmail.com>
2024-11-14 21:26:56 +0900

    codecrafters submit [skip ci]

--- src/replication_config.rs
+++ src/replication_config.rs
@@ -1,4 +1,6 @@
 use crate::protocol_constants::{BULK_STRING_PREFIX, CRLF};
+use rand::distr::Alphanumeric;
+use rand::Rng;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
@@ -7,15 +9,28 @@ pub struct ReplicationConfig {
     role: Arc<RwLock<String>>,
     master_host: Arc<RwLock<Option<String>>>,
     master_port: Arc<RwLock<Option<u16>>>,
+    master_replid: Arc<RwLock<String>>,
+    master_repl_offset: Arc<RwLock<u64>>,
 }
 
 impl ReplicationConfig {
     pub fn new() -> Self {
+        let replid = Self::generate_replication_id();
         Self {
             role: Arc::new(RwLock::new("master".to_string())),
             master_host: Arc::new(RwLock::new(None)),
             master_port: Arc::new(RwLock::new(None)),
+            master_replid: Arc::new(RwLock::new(replid)),
+            master_repl_offset: Arc::new(RwLock::new(0)),
         }
+    }
+
+    fn generate_replication_id() -> String {
+        rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(40)
+            .map(char::from)
+            .collect()
     }
 
     pub async fn set_replica_of(&self, host: String, port: u16) {
@@ -34,6 +49,8 @@ impl ReplicationConfig {
         *master_host = None;
         let mut master_port = self.master_port.write().await;
         *master_port = None;
+        let mut master_repl_offset = self.master_repl_offset.write().await;
+        *master_repl_offset = 0;
     }
 
     pub async fn get_role(&self) -> String {
@@ -43,6 +60,14 @@ impl ReplicationConfig {
     pub async fn get_replication_info(&self) -> String {
         let role = self.get_role().await;
         let mut info = format!("{}role:{}{}", BULK_STRING_PREFIX, role, CRLF);
+
+        if role == "master" {
+            let master_replid = self.master_replid.read().await.clone();
+            let master_repl_offset = *self.master_repl_offset.read().await;
+            info.push_str(&format!("master_replid:{}{}", master_replid, CRLF));
+            info.push_str(&format!("master_repl_offset:{}{}", master_repl_offset, CRLF));
+        }
+
         info
     }
 }
